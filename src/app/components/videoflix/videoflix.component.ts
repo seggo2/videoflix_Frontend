3b909258c8684b3fc5718a2ba8fc09837511430b
Sefa Gür <sefa_guer@hotmail.com>
2024-05-05 21:36:54 +0200

    component videoflix created finished design in login and register

---
+++ src/app/components/videoflix/videoflix.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-videoflix',
+  standalone: true,
+  imports: [],
+  templateUrl: './videoflix.component.html',
+  styleUrl: './videoflix.component.scss'
+})
+export class VideoflixComponent {
+
+}
